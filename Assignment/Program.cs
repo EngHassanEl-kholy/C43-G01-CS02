1e4d7918fff7fa69f7bdcd20a0f805b70e2fd7aa
Hassan <hassansayedelkholy@gmail.com>
2024-12-04 16:41:41 +0200

    Q01 Solution

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q01
+            //1-Write a program that allows the user to enter a number then print it.
+            //Console.WriteLine("Enter A Number");
+            //int Number;
+            //bool flag= int.TryParse(Console.ReadLine(), out Number);
+            //if (flag)
+            //    Console.WriteLine(Number);
+            //else
+            //    Console.WriteLine("Please Enter A Number"); 
+            #endregion
+
         }
     }
 }
