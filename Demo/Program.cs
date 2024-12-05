38334489626fd1f28dc8b001f8c63e7196ca3da6
Hassan <hassansayedelkholy@gmail.com>
2024-12-05 09:43:24 +0200

    Reference Type

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -29,6 +29,17 @@
             //Num++;        // Num=6     X=5
             #endregion
 
+            #region Reference Type
+            //string name; 
+            ////CLR will allocate 4 bytes at stack for the reference (name) which contain on address
+            //name = "Hassan";
+            //string name2 = name;
+            ////pass the reference of name = > name & name2 refer to same object
+            //name = "Ahmed";
+            //name refer to new object ("Ahmed")
+            //name2 still refer to same object ("Hassan")
+            #endregion
+
 
 
         }
