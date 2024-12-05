041b60f808ed24ad64a3dccfcf6b84072f5bda97
Hassan <hassansayedelkholy@gmail.com>
2024-12-05 10:57:50 +0200

    Q02 Solution

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -15,6 +15,21 @@
             //    Console.WriteLine("Please Enter A Number"); 
             #endregion
 
+            #region Q02
+            //2-Write C# program that converts a string to an integer,
+            //but the string contains non-numeric characters. And mention what will happen 
+            //Console.Write("Enter Msg : ");
+            //string msg = Console.ReadLine();
+            //int Num;
+            //if(int.TryParse(msg,out Num))
+            //    Console.WriteLine("string contains numeric characters");
+            //else
+            //    Console.WriteLine("string contains non-numeric characters");
+            //when user enter non-numeric characters int.TryParse(msg,out Num) will return false and execute else statement
+            
+
+            #endregion
+
         }
     }
 }
