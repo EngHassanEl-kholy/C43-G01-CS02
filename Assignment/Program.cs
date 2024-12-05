fc02f8ded622f800874ddd8efd382f1aee709699
Hassan <hassansayedelkholy@gmail.com>
2024-12-05 11:20:19 +0200

    Q03 Solution

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -26,8 +26,18 @@
             //else
             //    Console.WriteLine("string contains non-numeric characters");
             //when user enter non-numeric characters int.TryParse(msg,out Num) will return false and execute else statement
-            
 
+
+            #endregion
+
+            #region Q03
+            ////3-Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+            //Console.Write("Enter first Number");
+            //double Num01=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter second Number");
+            //float Num02 = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Sum {Num01} + {Num02} = {Num01+Num02}");
+            //floating number must declare = > float num=Value F; to make the compiler understand that is float
             #endregion
 
         }
